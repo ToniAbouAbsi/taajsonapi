fff047c31b3738fa64e7e8458ceddc2e126d80e1
ToniAbouAbsi <81428375+ToniAbouAbsi@users.noreply.github.com>
2022-11-25 18:11:18 +0200

    First Commit

--- src/Products.jsx
+++ src/Products.jsx
@@ -5,7 +5,7 @@ const Products = () => {
   const [data, setData] = useState([]);
 
   const getData = () => {
-    fetch('https://taajsonapi.herokuapp.com/api', {
+    fetch('  http://localhost:8000/products', {
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
@@ -24,30 +24,34 @@ const Products = () => {
   return (
     <div className='App'>
       <table>
-        <tr>
-          <th>
-            <h2>ID</h2>
-          </th>
-          <th>
-            <h2>Product Name</h2>
-          </th>
-          <th>
-            <h2>Price</h2>
-          </th>
-          <th>
-            <h2>Quantity</h2>
-          </th>
-        </tr>
-        {data.map((item) => {
-          return (
-            <tr key={item.id}>
-              <td>#{item.id}</td>
-              <td>{item.productName}</td>
-              <td>{item.price}</td>
-              <td>{item.quantity}</td>
-            </tr>
-          );
-        })}
+        <thead>
+          <tr>
+            <th>
+              <h2>ID</h2>
+            </th>
+            <th>
+              <h2>Product Name</h2>
+            </th>
+            <th>
+              <h2>Price</h2>
+            </th>
+            <th>
+              <h2>Quantity</h2>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => {
+            return (
+              <tr key={item.id}>
+                <td>#{item.id}</td>
+                <td>{item.productName}</td>
+                <td>{item.price}</td>
+                <td>{item.quantity}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
